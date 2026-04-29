afc4146b508d0d74ddbdb9ebd164cc6e495abc76
siddiquiatif17 <152960230+siddiquiatif17@users.noreply.github.com>
2026-04-29 14:13:43 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,36 +1,34 @@
 class Solution {
 public:
-    void dfs(vector<vector<int>>& graph,int node,vector<int>& visited){
+    void dfs(int node,vector<vector<int>>& graph,vector<int>& visited){
         visited[node]=1;
         for(int neigh:graph[node]){
             if(!visited[neigh]){
-                dfs(graph,neigh,visited);
+                dfs(neigh,graph,visited);
             }
         }
     }
     int findCircleNum(vector<vector<int>>& isConnected) {
         int n=isConnected.size();
         vector<vector<int>> graph(n);
+
         for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                if(isConnected[i][j]==1){
-                    graph[i].push_back(j);
-                    graph[j].push_back(i);
-                }
+            for(int j=0;j<isConnected[i].size();i++){
+             if(isConnected[i][j]==1){
+                   graph[i].push_back(j);
+                graph[j].push_back(i);
+             }
             }
         }
+        int count=0;
         vector<int> visited(n,0);
-        int cnt=0;
-
         for(int i=0;i<n;i++){
             if(!visited[i]){
-                dfs(graph,i,visited);
-                cnt++;
+                count++;
+                dfs(i,graph,visited);
             }
         }
-        return cnt;
-
-
+    return count;
         
     }
 };
