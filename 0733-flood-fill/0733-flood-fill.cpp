f959d584d4c0127621363279ac0b38ded09d6624
siddiquiatif17 <152960230+siddiquiatif17@users.noreply.github.com>
2026-04-29 14:39:07 +0530

    Time: 1 ms (20.14%), Space: 18.2 MB (43.35%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
-   int dx[4]={0,-1,0,1};
+    int dx[4]={0,-1,0,1};
     int dy[4]={-1,0,1,0};
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        if(image[sr][sc]==color)return image;
+        int original=image[sr][sc];
+        if(original==color)return image;
         int n=image.size();
         int m=image[0].size();
-        int originalColor=image[sr][sc];
-        queue<pair<int,int>> q;
+        
+        queue<pair<int,int>>  q;
         q.push({sr,sc});
-
         while(!q.empty()){
             auto temp=q.front();
             q.pop();
@@ -19,13 +19,12 @@ public:
             for(int k=0;k<4;k++){
                 int newRow=row+dx[k];
                 int newCol=col+dy[k];
-                if(newRow>=0 && newRow<n && newCol>=0 && newCol<m && image[newRow][newCol]==originalColor){
+                if(newRow>=0 && newRow<n && newCol>=0 && newCol<m && image[newRow][newCol]==original){
                     q.push({newRow,newCol});
-                    // image[newRow][newCol]==color;
+                    
                 }
             }
         }
         return image;
-        
     }
 };
