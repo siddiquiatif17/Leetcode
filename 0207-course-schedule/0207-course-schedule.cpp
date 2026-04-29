1fa18f51a6170e7e0ecaf05a7162eca8426dbb38
siddiquiatif17 <152960230+siddiquiatif17@users.noreply.github.com>
2026-04-29 14:52:58 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -2,28 +2,31 @@ class Solution {
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
         int n=prerequisites.size();
-        queue<int> q;
+        vector<int> indegree(numCourses,0);
         vector<vector<int>> graph(numCourses);
-        vector<int> indegree(numCourses);
         for(int i=0;i<n;i++){
             int u=prerequisites[i][0];
-            int v=prerequisites[i][1];
-            graph[u].push_back(v);
-            indegree[v]++;
+                int v=prerequisites[i][1];
+                graph[u].push_back(v);
+                
+                indegree[v]++;
         }
+
+        queue<int> q;
         for(int i=0;i<numCourses;i++){
             if(indegree[i]==0)q.push(i);
         }
+
         while(!q.empty()){
             int node=q.front();
             q.pop();
-            for(int nbr:graph[node]){
-                indegree[nbr]--;
-                if(indegree[nbr]==0)q.push(nbr);
+            for(int neigh:graph[node]){
+                indegree[neigh]--;
+                if(indegree[neigh]==0)q.push(neigh);
             }
         }
         for(int i=0;i<numCourses;i++){
-            if(indegree[i]!=0)return false;
+            if(!indegree[i])return false;
         }
         return true;
     }
