ccd4f657ad09a58367e0601f7390a1cd11aafe2c
siddiquiatif17 <152960230+siddiquiatif17@users.noreply.github.com>
2026-05-06 14:45:31 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,21 +1,29 @@
 class Solution {
 public:
-    bool sol(vector<int>& nums,int idx,int target,vector<vector<int>>& dp){
-        if(target==0)return true;
-        if(idx==nums.size())return false;
-        if(dp[idx][target]!=-1)return dp[idx][target];
-        bool take = false;
-        if(nums[idx] <= target)
-            take = sol(nums, idx+1, target - nums[idx], dp);
-        bool notTake=sol(nums,idx+1,target,dp);
-        return dp[idx][target]= take || notTake;
+    int sol(vector<int>& nums,int i,int target,vector<vector<int>>& dp){
+        int n=nums.size();
+        if(target==0)return 1;
+        if(i>=n)return 0;
+
+        if(dp[i][target]!=-1)return dp[i][target];
+
+        int pick = 0;
+        if(nums[i] <= target) {
+            pick = sol(nums, i+1, target - nums[i], dp);
+        }
+
+        int notPick=sol(nums,i+1,target,dp);
+        return dp[i][target]= pick || notPick;
+
     }
     bool canPartition(vector<int>& nums) {
         int n=nums.size();
         int sum=0;
         for(int i=0;i<n;i++)sum+=nums[i];
-        if(sum%2==1)return false;
+        if(sum%2!=0)return false;
         vector<vector<int>> dp(n,vector<int>((sum/2)+1,-1));
+
+
         return sol(nums,0,sum/2,dp);
         
     }
