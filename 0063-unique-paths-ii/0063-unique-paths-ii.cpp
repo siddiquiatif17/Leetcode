3b5d6a3c1bc902c202abb05384d2de6742906712
siddiquiatif17 <152960230+siddiquiatif17@users.noreply.github.com>
2026-05-06 14:27:47 +0530

    Time: 0 ms (100%), Space: 11.9 MB (28.92%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -1,16 +1,14 @@
 class Solution {
 public:
 int m,n;
-    int sol(vector<vector<int>>& grid,int i,int j,vector<vector<int>>& dp){
-        if(i==m || j==n)return 0;
-        if(grid[i][j]==1)return  0;
-        if(i==m-1 && j==n-1)return 1;
-        if(dp[i][j]!=-1)return dp[i][j];
-        int down=sol(grid,i+1,j,dp);
-        int right=sol(grid,i,j+1,dp);
-        return dp[i][j]= down+right;
-
-    }
+int sol(vector<vector<int>>& grid,int i ,int j,vector<vector<int>>& dp){
+    if(i==m-1 && j==n-1)return (grid[i][j]==1)?0:1;
+    if(i>=m || j>=n || grid[i][j]==1)return 0;
+if(dp[i][j]!=-1)return dp[i][j];
+    int down=sol(grid,i+1,j,dp);
+    int right=sol(grid,i,j+1,dp);
+    return dp[i][j]= down+right;
+}
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
         m=obstacleGrid.size();
         n=obstacleGrid[0].size();
