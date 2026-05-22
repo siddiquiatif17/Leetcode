04385859458059e700235298c931f1483ee5a62b
siddiquiatif17 <152960230+siddiquiatif17@users.noreply.github.com>
2026-05-22 18:28:27 +0530

    Time: 0 ms (100%), Space: 19.1 MB (80.85%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -2,21 +2,19 @@ class Solution {
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
         int n=prerequisites.size();
-        vector<int> indegree(numCourses);
         vector<vector<int>> graph(numCourses);
+        vector<int> indegree(numCourses,0);
         for(int i=0;i<n;i++){
             int u=prerequisites[i][0];
-                int v=prerequisites[i][1];
-                graph[u].push_back(v);
-                
-                indegree[v]++;
+            int v=prerequisites[i][1];
+            graph[v].push_back(u);
+            indegree[u]++;
         }
-
         queue<int> q;
         for(int i=0;i<numCourses;i++){
             if(indegree[i]==0)q.push(i);
         }
-
+        // vector<int> topo;
         while(!q.empty()){
             int node=q.front();
             q.pop();
@@ -29,5 +27,6 @@ public:
             if(indegree[i]!=0)return false;
         }
         return true;
+        
     }
 };
