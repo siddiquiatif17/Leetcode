d458c4433d090db09917fec716dce481f59004e9
siddiquiatif17 <152960230+siddiquiatif17@users.noreply.github.com>
2026-05-25 14:12:16 +0530

    Time: 7 ms (72.47%), Space: 42 MB (91.6%) - LeetHub

---
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
@@ -0,0 +1,44 @@
+class DSU{
+    public:
+    vector<int> parent,size;
+
+    DSU(int n){
+    parent.resize(n+1);
+    size.resize(n+1,1);
+    for(int i=0;i<=n;i++)parent[i]=i;
+    }
+
+    int find(int x){
+        if(parent[x]==x)return x;
+        return parent[x]=find(parent[x]);
+    }
+
+    bool Union(int a,int b){
+        a=find(a);
+        b=find(b);
+        if(a==b)return true;
+        if(size[a]<size[b])swap(a,b);
+        parent[b]=a;
+        size[a]+=size[b];
+        return false;
+     }
+};
+class Solution {
+public:
+    int makeConnected(int n, vector<vector<int>>& connections) {
+        int m=connections.size();
+         if(m < n - 1) return -1;
+        DSU dsu(n);
+        int cnt=0;
+        for(int i=0;i<m;i++){
+            int u=connections[i][0];
+            int v=connections[i][1];
+            dsu.Union(u,v);
+        }
+        for(int i=0;i<n;i++){
+            if(dsu.find(i)==i)cnt++;
+        }
+        return cnt-1;
+        
+    }
+};
