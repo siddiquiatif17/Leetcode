74a6a96d46ea990cc3dbd0b4f786e734092109ae
siddiquiatif17 <152960230+siddiquiatif17@users.noreply.github.com>
2026-06-15 23:55:08 +0530

    Time: 73 ms (90.06%), Space: 44.7 MB (42.64%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.cpp
+++ 0743-network-delay-time/0743-network-delay-time.cpp
@@ -6,37 +6,39 @@ public:
         for(int i=0;i<m;i++){
             int u=times[i][0];
             int v=times[i][1];
-            int time=times[i][2];
-            graph[u].push_back({v,time});
+            int wt=times[i][2];
+            graph[u].push_back({v,wt});
         }
 
-        vector<int> dist(n+1,1e9);
+        vector<int> dist(n+1,INT_MAX);
         dist[k]=0;
+
         set<pair<int,int>> st;
         st.insert({0,k});
         while(!st.empty()){
-            auto it=*st.begin();
-            st.erase(st.begin());
-            int node=it.second;
-            int wt=it.first;
-            for(auto it:graph[node]){
-                int neigh=it.first;
-                int neighWt=it.second;
-                if(wt+neighWt<dist[neigh]){
-                    if(dist[neigh]!=1e9){
-                        st.erase({dist[neigh],neigh});
+            auto temp=*st.begin();
+            st.erase(temp);
+            int time=temp.first;
+            int node=temp.second;
+            for(auto neigh:graph[node]){
+                int neighNode=neigh.first;
+                int neighDist=neigh.second;
+                if(time+neighDist<INT_MAX){
+                    if(time+neighDist<dist[neighNode]){
+                        dist[neighNode]=time+neighDist;
+                        st.insert({dist[neighNode],neighNode});
                     }
-                        dist[neigh]=wt+neighWt;
-                        st.insert({dist[neigh],neigh});
                 }
             }
         }
-        int ans=0;
         for(int i=1;i<=n;i++){
-            if(dist[i]==1e9)return -1;
-            ans=max(ans,dist[i]);
+            if(dist[i]==INT_MAX)return -1;
         }
-        return ans;
+        int maxi=INT_MIN;
+        for(int i=1;i<=n;i++){
+            maxi=max(maxi,dist[i]);
+        }
+        return maxi;
         
     }
 };
