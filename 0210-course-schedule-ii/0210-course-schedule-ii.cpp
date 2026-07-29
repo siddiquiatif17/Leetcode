c36b641f7939aa1b00c71d40dac5ca18aeb00df9
siddiquiatif17 <152960230+siddiquiatif17@users.noreply.github.com>
2026-07-29 10:19:26 +0530

    Time: 1 ms (80.04%), Space: 18.2 MB (83.14%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.cpp
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -3,30 +3,30 @@ public:
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
         int n=prerequisites.size();
         vector<vector<int>> graph(numCourses);
-        vector<int> indegree(numCourses);
+        vector<int> indegree(numCourses,0);
+
         for(int i=0;i<n;i++){
             int u=prerequisites[i][0];
             int v=prerequisites[i][1];
             graph[v].push_back(u);
             indegree[u]++;
         }
+
         queue<int> q;
         for(int i=0;i<numCourses;i++){
             if(indegree[i]==0)q.push(i);
         }
-        vector<int> topo;
+        vector<int> ans;
         while(!q.empty()){
-            int  node=q.front();
+            int node=q.front();
             q.pop();
-            topo.push_back(node);
+            ans.push_back(node);
             for(int neigh:graph[node]){
                 indegree[neigh]--;
                 if(indegree[neigh]==0)q.push(neigh);
             }
         }
-        //  if(topo.size() != numCourses)
-        //     return {};
-        return (topo.size()!=numCourses)?vector<int>{}:topo;
-        
+         for(int i=0;i<numCourses;i++)if(indegree[i]!=0)return {};
+        return ans;
     }
 };
