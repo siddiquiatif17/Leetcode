5e867fc7382bcdabd0297e7302fe1556cc0141ba
siddiquiatif17 <152960230+siddiquiatif17@users.noreply.github.com>
2026-07-31 17:52:25 +0530

    Time: 3 ms (26.04%), Space: 11.3 MB (17.33%) - LeetHub

---
+++ 0046-permutations/0046-permutations.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<vector<int>> ans;
+    void sol(vector<int> &temp,vector<int> &nums,set<int> &st){
+        if(temp.size()==nums.size()){
+            ans.push_back(temp);
+            return;
+        }
+        for(int i=0;i<nums.size();i++){
+            
+            if(st.find(nums[i])==st.end()){
+                temp.push_back(nums[i]);
+                st.insert(nums[i]);
+                sol(temp,nums,st);
+                temp.pop_back();
+                st.erase(nums[i]);
+            }
+        }
+    }
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<int> temp;
+        set<int> st;
+        sol(temp,nums,st);
+        return ans;
+    }
+};
