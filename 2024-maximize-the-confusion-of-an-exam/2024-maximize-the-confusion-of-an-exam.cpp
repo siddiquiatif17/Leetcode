6f3498e3654327c52672df3f41f5590a159f9b62
siddiquiatif17 <152960230+siddiquiatif17@users.noreply.github.com>
2026-08-01 23:00:32 +0530

    Time: 7 ms (69.79%), Space: 12.5 MB (78.91%) - LeetHub

---
+++ 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int sol(string &s,int k,char ch){
+        int n=s.size();
+        int j=0;
+        int cnt=0;
+        int ans=0;
+        for(int i=0;i<n;i++){
+            if(s[i]==ch)cnt++;
+            while(cnt>k){
+                if(s[j]==ch)cnt--;
+                j++;
+            }
+            ans=max(ans,i-j+1);
+        }
+        return ans;
+    }
+    int maxConsecutiveAnswers(string answerKey, int k) {
+        return max(sol(answerKey,k,'F'),sol(answerKey,k,'T'));
+        
+    }
+};
