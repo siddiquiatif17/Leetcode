de32390e6d2bde4ecd3b4772ba5a2dba666291fe
siddiquiatif17 <152960230+siddiquiatif17@users.noreply.github.com>
2026-08-01 15:15:03 +0530

    Time: 5 ms (72.27%), Space: 11 MB (78.17%) - LeetHub

--- 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.cpp
+++ 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.cpp
@@ -4,14 +4,17 @@ public:
         int n=s.size();
      
         int j=0;
-        unordered_map<char,int> mpp2;
+        // unordered_map<char,int> mpp2;
+        int cnt[3]={0};
         int ans=0;
         for(int i=0;i<n;i++){
-            mpp2[s[i]]++;
-            while(mpp2.size()==3){
+            // mpp2[s[i]]++;
+            cnt[s[i]-'a']++;
+            while(cnt[0] && cnt[1] && cnt[2]){
                 ans+=n-i;
-                mpp2[s[j]]--;
-                if(mpp2[s[j]]==0)mpp2.erase(s[j]);
+                // mpp2[s[j]]--;
+                cnt[s[j]-'a']--;
+                // if(mpp2[s[j]]==0)mpp2.erase(s[j]);
                 j++;
             }
         }
