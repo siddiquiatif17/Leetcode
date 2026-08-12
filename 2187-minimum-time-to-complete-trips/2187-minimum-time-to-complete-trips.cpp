c8979f266d4f245670633894bc34fbdfb6eea006
siddiquiatif17 <152960230+siddiquiatif17@users.noreply.github.com>
2026-08-12 21:05:35 +0530

    Time: 81 ms (16.64%), Space: 98.3 MB (89.08%) - LeetHub

---
+++ 2187-minimum-time-to-complete-trips/2187-minimum-time-to-complete-trips.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public: 
+    bool possible(long long k,vector<int>& time,int totalTrips){
+        long long count=0;
+        for(int i=0;i<time.size();i++){
+            count+=k/time[i];
+            if(count>=totalTrips)return true;
+        }
+        return false;
+    }
+    long long minimumTime(vector<int>& time, int totalTrips) {
+        int n=time.size();
+        sort(time.begin(),time.end());
+        long long low=1;
+        long long mini=*min_element(time.begin(),time.end());
+        long long high=1LL*mini*totalTrips;
+        long long ans=INT_MAX;
+        while(low<=high){
+            long long mid=low+(high-low)/2;
+            if(possible(mid,time,totalTrips)){
+                ans=mid;
+                high=mid-1;
+            }else low=mid+1;    
+        }
+        return ans;
+        
+    }
+};
