ce916bb7dd5638ee128d589ede11d73f83b265a9
siddiquiatif17 <152960230+siddiquiatif17@users.noreply.github.com>
2026-08-17 10:40:40 +0530

    Time: 67 ms (20.16%), Space: 75.1 MB (11.39%) - LeetHub

--- 1514-path-with-maximum-probability/1514-path-with-maximum-probability.cpp
+++ 1514-path-with-maximum-probability/1514-path-with-maximum-probability.cpp
@@ -1,39 +1,33 @@
 class Solution {
 public:
-    double maxProbability(int n, vector<vector<int>>& edges, vector<double>& succProb, int start_node, int end_node) {
+    double maxProbability(int n, vector<vector<int>>& edges, vector<double>& succProb, int start, int end) {
         int size=edges.size();
         vector<vector<pair<int,double>>> graph(n);
         for(int i=0;i<size;i++){
             int u=edges[i][0];
             int v=edges[i][1];
-            double prob=succProb[i];
-            graph[u].push_back({v,prob});
-            graph[v].push_back({u,prob});
+            double weight=succProb[i];
+            graph[u].push_back({v,weight});
+            graph[v].push_back({u,weight});
         }
 
-       set<pair<double,int>, greater<pair<double,int>>> st;
-        st.insert({1.0,start_node});
-        vector<double> probArr(n,0.0000);
-        probArr[start_node]=1.0;
-
+        set<pair<double,int>, greater<pair<double,int>>> st;
+        st.insert({1.0,start});
+        vector<double> prob(n,0.0);
         while(!st.empty()){
-           auto temp=*st.begin();
-
-            st.erase(st.begin());
-            double prob = temp.first;
-    int node = temp.second;
-
-             if(prob < probArr[node]) continue;
-
+            auto temp=*st.begin();
+            st.erase(*st.begin());
+            int node=temp.second;
+            double wt=temp.first;
             for(auto neigh:graph[node]){
                 int neighNode=neigh.first;
-                double neighProb=neigh.second;
-                if(neighProb*prob>probArr[neighNode]){
-                    probArr[neighNode]=neighProb*prob;
-                    st.insert({neighProb*prob,neighNode});
+                double neighWt=neigh.second;
+                if(wt*neighWt>prob[neighNode]){
+                    prob[neighNode]=wt*neighWt;
+                    st.insert({wt*neighWt,neighNode});
                 }
             }
         }
-        return probArr[end_node];
+        return prob[end];
     }
 };
