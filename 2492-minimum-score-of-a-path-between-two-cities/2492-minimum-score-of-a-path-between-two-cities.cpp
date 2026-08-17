0ac66975b34f6822c1d9d11dc1481db042780ae6
siddiquiatif17 <152960230+siddiquiatif17@users.noreply.github.com>
2026-08-17 22:27:20 +0530

    Time: 87 ms (30.92%), Space: 176.3 MB (5.04%) - LeetHub

--- 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.cpp
+++ 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.cpp
@@ -1,32 +1,39 @@
 class Solution {
 public:
     int minScore(int n, vector<vector<int>>& roads) {
-        int m=roads.size();
+
+        int size=roads.size();
         vector<vector<pair<int,int>>> graph(n+1);
-        for(int i=0;i<m;i++){
+        for(int i=0;i<size;i++){
             int u=roads[i][0];
             int v=roads[i][1];
             int wt=roads[i][2];
             graph[u].push_back({v,wt});
             graph[v].push_back({u,wt});
         }
+
+        
+        queue<pair<int,int>> q;
+        q.push({0,1});
         vector<int> visited(n+1,0);
-        queue<int> q;
-           q.push(1);
-           visited[1]=1;
-    int ans=INT_MAX;
+        visited[1]=1;
+        int ans=INT_MAX;
         while(!q.empty()){
-            int  node=q.front();
+            auto temp=q.front();
             q.pop();
-            for(auto &[neigh, wt] : graph[node]){
-                ans=min(ans,wt);
-                if(!visited[neigh]){
-                    visited[neigh]=1;
-                    q.push(neigh);
+            int node=temp.second;
+            int wt=temp.first;
+           
+            for(auto neigh:graph[node]){
+                int neighNode=neigh.first;
+                int neighWt=neigh.second;
+                 ans=min(ans,neighWt);
+                if(!visited[neighNode]){
+                    visited[neighNode]=1;
+                    q.push({neighWt,neighNode});
                 }
             }
         }
         return ans;
-        
     }
 };
