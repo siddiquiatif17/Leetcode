a607d2d2016a750d0fe324525a09f7e43ebe72c3
siddiquiatif17 <152960230+siddiquiatif17@users.noreply.github.com>
2026-08-17 10:21:03 +0530

    Time: 0 ms (100%), Space: 14.8 MB (26.57%) - LeetHub

--- 0841-keys-and-rooms/0841-keys-and-rooms.cpp
+++ 0841-keys-and-rooms/0841-keys-and-rooms.cpp
@@ -1,24 +1,22 @@
 class Solution {
 public:
-    void dfs(vector<vector<int>>& graph,int node,vector<int>& visited){
-        visited[node]=1;
-        for(int neigh:graph[node]){
-            if(!visited[neigh]){
-                dfs(graph,neigh,visited);
-            }
-        }
-    }
     bool canVisitAllRooms(vector<vector<int>>& rooms) {
         int n=rooms.size();
-        
 
         vector<int> visited(n,0);
-        dfs(rooms,0,visited);
+        queue<int> q;
+        q.push(0);
+        while(!q.empty()){
+            int node=q.front();
+            q.pop();
+            visited[node]=1;
+            for(int neigh:rooms[node]){
+               if(!visited[neigh])q.push(neigh);
+            }
+        }
         for(int i=0;i<n;i++){
             if(!visited[i])return false;
         }
         return true;
-    
-        
     }
 };
