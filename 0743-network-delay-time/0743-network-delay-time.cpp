f2bc36d7aa229bff9c52746679d8dac534f1f39b
siddiquiatif17 <152960230+siddiquiatif17@users.noreply.github.com>
2026-09-03 19:21:07 +0530

    Time: 73 ms (88.38%), Space: 44.7 MB (44.46%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.cpp
+++ 0743-network-delay-time/0743-network-delay-time.cpp
@@ -1,44 +1,44 @@
 class Solution {
 public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-        int m=times.size();
         vector<vector<pair<int,int>>> graph(n+1);
+        int m=times.size();
+
         for(int i=0;i<m;i++){
             int u=times[i][0];
             int v=times[i][1];
-            int wt=times[i][2];
-            graph[u].push_back({v,wt});
+            int time=times[i][2];
+            graph[u].push_back({v,time});
         }
-
-        vector<int> dist(n+1,INT_MAX);
-        dist[k]=0;
 
         set<pair<int,int>> st;
         st.insert({0,k});
+        vector<int> dist(n+1,INT_MAX);
+        dist[k]=0;
+
         while(!st.empty()){
             auto temp=*st.begin();
-            st.erase(temp);
-            int time=temp.first;
+            st.erase(st.begin());
             int node=temp.second;
+            int time=temp.first;
+
             for(auto neigh:graph[node]){
                 int neighNode=neigh.first;
-                int neighDist=neigh.second;
-                if(time+neighDist<INT_MAX){
-                    if(time+neighDist<dist[neighNode]){
-                        dist[neighNode]=time+neighDist;
-                        st.insert({dist[neighNode],neighNode});
-                    }
+                int neighTime=neigh.second;
+                if(time+neighTime<dist[neighNode]){
+                    dist[neighNode]=time+neighTime;
+                    st.insert({time+neighTime,neighNode});
                 }
             }
         }
+        int ans=0;
+     
         for(int i=1;i<=n;i++){
             if(dist[i]==INT_MAX)return -1;
+            ans=max(ans,dist[i]);
         }
-        int maxi=INT_MIN;
-        for(int i=1;i<=n;i++){
-            maxi=max(maxi,dist[i]);
-        }
-        return maxi;
+        return ans;
+        
         
     }
 };
